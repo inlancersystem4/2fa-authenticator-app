e20c66bc57f5f5da52829b591c2c6689718643be
kasundra jeet <132254157+inlancersystem4@users.noreply.github.com>
2025-01-17 16:57:15 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { Toaster } from "sonner";
 import "./styles/index.css";
+import "./styles/font.css";
 import QueryProvider from "./context/QueryProvider";
 import App from "./app";
 
@@ -8,6 +10,7 @@ const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <QueryProvider>
+    <Toaster richColors  closeButton position="top-right" />
     <BrowserRouter>
       <Routes>
         <Route index element={<App />} />
